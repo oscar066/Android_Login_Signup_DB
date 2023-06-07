ec8544dbac26222ea7fef0bac45337a8207b771f
oscar066 <oscarkaruga1@gmail.com>
2023-06-07 14:51:29 +0300

    Login and SignUp

---
+++ app/src/main/java/com/example/myloginapp/URLs.java
@@ -0,0 +1,10 @@
+package com.example.myloginapp;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://10.5.33.51/Android/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
